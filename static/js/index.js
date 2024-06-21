59bc0a1602282ad020ae3a56021d85c3557f82d8
ArthurMTX <itsmitralyx@gmail.com>
2024-06-21 16:18:19 +0200

    - Index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,3 @@
-// static/js/index.js
-
 // Initialize the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
