3384158980dffdda2677a659a544a6b987759a93
ArthurMTX <itsmitralyx@gmail.com>
2024-06-24 11:21:04 +0200

    - Index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,50 +1,30 @@
-// static/js/index.js
-import * as THREE from 'three';
-import { OBJLoader } from 'OBJLoader';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'ScrollTrigger';
-
 gsap.registerPlugin(ScrollTrigger);
 
-// Initialize the scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('scene-container').appendChild(renderer.domElement);
+const video = document.querySelector("video");
 
-// Load the OBJ model
-const objLoader = new OBJLoader();
-objLoader.load('/static/assets/macbook.obj', (object) => {
-    object.scale.set(2, 2, 2); // Scale the model if necessary
-    scene.add(object);
-
-    // Animation for OBJ model rotation based on scroll
-    gsap.to(object.rotation, {
-        scrollTrigger: {
-            trigger: ".content",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: true
-        },
-        x: Math.PI * 2,
-        y: Math.PI * 2,
-        duration: 1
-    });
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "video",
+    start: "top top",
+    end: "bottom+=200% bottom",
+    scrub: true,
+    markers: true
+  }
 });
 
-camera.position.z = 5;
+video.onloadedmetadata = function () {
+  tl.to(video, { currentTime: video.duration });
+};
 
-// Render loop
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+// Dealing with devices
+function isTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
 }
-animate();
-
-// Handle window resize
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+if (isTouchDevice()) {
+  video.play();
+  video.pause();
+}
